27f0ffaa23639f150dbb09f04656ebf84ef13e8f
Daniel Gimenes <daniel@danielgimenes.com.br>
2022-10-14 17:25:30 -0300

    handle disconnected empty state

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading, SimpleGrid, Stack } from "@chakra-ui/react";
+import { Center, Container, Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
 import { supportedTokens } from "@voting-power/sdk";
 import { ConnectKitButton } from "connectkit";
 import { Suspense } from "react";
@@ -8,24 +8,34 @@ import { TokenCard } from "../components/TokenCard";
 export default function IndexPage() {
   const account = useAccount();
 
-  return (
-    <>
-      <Stack as={Container} maxW={"5xl"} p="6" spacing={8}>
-        <Stack direction="row" justifyContent="space-between" alignItems={"center"}>
-          <Heading fontSize={"2xl"}>⚡️Voting Power Tokens</Heading>
+  if (!account.address) {
+    return (
+      <Center w={"full"} h="100vh">
+        <Stack align={"center"} spacing={6}>
+          <Heading as="h2" size="xl">
+            ⚡️Voting Power
+          </Heading>
+          <Text color={"gray.400"}>Connect your wallet to see your voting power tokens.</Text>
           <ConnectKitButton />
         </Stack>
+      </Center>
+    );
+  }
 
-        {account.address && (
-          <SimpleGrid columns={{ base: 1 }} spacing={6} alignItems="start">
-            <Suspense fallback={null}>
-              {supportedTokens.map((token, key) => (
-                <TokenCard key={key} token={token}></TokenCard>
-              ))}
-            </Suspense>
-          </SimpleGrid>
-        )}
+  return (
+    <Stack as={Container} maxW={"5xl"} p="6" spacing={8}>
+      <Stack direction="row" justifyContent="space-between" alignItems={"center"}>
+        <Heading fontSize={"2xl"}>⚡️Voting Power</Heading>
+        <ConnectKitButton />
       </Stack>
-    </>
+
+      <SimpleGrid columns={{ base: 1 }} spacing={6} alignItems="start">
+        <Suspense fallback={null}>
+          {supportedTokens.map((token, key) => (
+            <TokenCard key={key} token={token}></TokenCard>
+          ))}
+        </Suspense>
+      </SimpleGrid>
+    </Stack>
   );
 }
