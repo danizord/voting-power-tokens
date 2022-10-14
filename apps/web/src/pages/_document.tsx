7e2f15e20472ae30f5ebcb7d03e7dd9f62a645af
Daniel Gimenes <daniel@danielgimenes.com.br>
2022-10-14 20:06:24 -0300

    add title

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -10,6 +10,8 @@ export default function Document() {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡️</text></svg>"
         />
+        {/*eslint-disable @next/next/no-title-in-document-head */}
+        <title>Voting Power</title>
       </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
